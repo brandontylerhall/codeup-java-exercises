f5bbf374a61535599d237f01c25f78eb665fa664
Brandon <bran.tyler.hall@gmail.com>
2023-01-02 17:36:56 -0500

    completed first part of exercise

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,7 @@
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        /*The value of pi is approximately 3.14.*/
+        System.out.printf("The value of pi is approximately %f", pi);
+    }
+}
