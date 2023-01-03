760235fd2bba952a5e46941f414bfb199dcca938
Brandon <bran.tyler.hall@gmail.com>
2023-01-03 17:57:55 -0500

    completed stringexercise class

---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String brickInTheWall = "We don't need no education\n" + "We don't need no thought control\n";
+        System.out.println(brickInTheWall);
+
+        String quoteInception = "Check \"this\" out!, \"s inside of \"s!\n";
+        System.out.println(quoteInception);
+
+        String windows = "In windows, the main drive is usually C:\\\n";
+        System.out.println(windows);
+
+        String backslashes = "I can do backslashes \\, double backslashes \\\\,\n" + "and the amazing triple backslash \\\\\\!";
+        System.out.println(backslashes);
+    }
+}
