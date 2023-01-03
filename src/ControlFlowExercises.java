9a8b2c44d519d7d8f289297808ab5f1c161cfebb
Brandon <bran.tyler.hall@gmail.com>
2023-01-03 18:41:03 -0500

    unsure why i am getting an infinite do-while loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,19 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
         /*while loop*/
-        int i = 5;
+//        int i = 5;
+//
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
 
-        while (i <= 15) {
-            System.out.println(i);
-            i++;
-        }
+        /*do while loop*/
+        int x = 0;
+        do {
+            x += 2;
+            System.out.println(x);
+        } while (x <= 100);
+
     }
 }
