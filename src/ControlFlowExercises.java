647511c1367a40f90e1f8783d54e414b4ad3de4e
Brandon <bran.tyler.hall@gmail.com>
2023-01-03 18:46:30 -0500

    turned loops into for loops. still getting infinite loop (idk why)

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,19 +1,18 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
         /*while loop*/
-//        int i = 5;
-//
-//        while (i <= 15) {
-//            System.out.println(i);
-//            i++;
-//        }
+        for (int i = 5; i <= 15; i++) {
+            System.out.println(i);
+        }
 
         /*do while loop*/
-        int x = 0;
-        do {
-            x += 2;
+        for (int x = 2; x < 1000000; x *= x) {
             System.out.println(x);
-        } while (x <= 100);
-
+        }
+//        int x = 2;
+//        do {
+//            System.out.println(x);
+//            x *= x;
+//        } while (x < 1000000);
     }
 }
