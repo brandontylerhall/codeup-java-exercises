27a766b649974c518cd56b57fd169886a61330ac
Brandon <bran.tyler.hall@gmail.com>
2023-01-04 15:12:32 -0500

    finished problem 3 and 4

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,25 +1,57 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        /*infinite loop. but y doe*/
-//        for (int x = 2; x < 1000000; x *= x) {
-//            System.out.println(x);
-//        }
-//        int x = 2;
-//        do {
-//            System.out.println(x);
-//            x *= x;
-//        } while (x < 1000000);
+        Scanner in = new Scanner(System.in);
 
-        for (int i = 1; i < 101; i++) {
-            if (i % 15 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else {
-                System.out.println(i);
+        String input2;
+        do {
+            input2 = "";
+            System.out.println("What number would you like to go up to?");
+
+            int input = in.nextInt();
+
+            System.out.println("Here is your table! \n");
+            System.out.printf(" Table of Powers %n");
+            System.out.printf("------------------------%n");
+            System.out.printf("%-6s | %-6s | %4s %n", "number", "squared", "cubed");
+            System.out.printf("%-6s | %-6s | %4s %n", "------", "-------", "-----");
+
+            for (int i = 1; i <= input; i++) {
+                System.out.printf("%-6d | %-7d | %-4d %n", i, i * i, i * i * i);
             }
-        }
+
+            System.out.println("Would you like to do another number? [y/n]");
+            input2 = in.next();
+
+            if (input2.equalsIgnoreCase("n")) {
+                System.out.println("Have a good day, then!");
+            }
+        } while (input2.equalsIgnoreCase("y"));
+
+        boolean confirmation;
+        do {
+            System.out.println("Input a numerical grade from 0 to 100");
+            int grade = in.nextInt();
+
+            if (grade >= 88) {
+                System.out.println("A");
+            } else if (grade >= 80) {
+                System.out.println("B");
+            } else if (grade >= 67) {
+                System.out.println("C");
+            } else if (grade >= 60) {
+                System.out.println("D");
+            } else {
+                System.out.println("F");
+            }
+            System.out.println("Would you like to do another number? [y/n]");
+            String input = in.next();
+            confirmation = input.equalsIgnoreCase("y");
+
+        } while (confirmation);
+        System.out.println("kthxbai");
+
+
     }
 }
