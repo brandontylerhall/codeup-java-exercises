2f8b991e3a90bd6af45a87f89b695faf0ba4a054
Brandon <bran.tyler.hall@gmail.com>
2023-01-10 12:05:21 -0500

    comleted circles

---
+++ src/ArraysExercises.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class ArraysExercises {
+    public static void main(String[] args) {
+        Person[] people = {new Person("Brandon"), new Person("Ricky"), new Person("Tiberius")};
+
+        for (Person person : people) {
+            System.out.println(people.getName());
+        }
+    }
+
+
+}
